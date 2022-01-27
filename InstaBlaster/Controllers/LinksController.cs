ea201143da738db39e1042394d25a6e3edd91075
Alexander Grebenkov <aleksander.grebenkov@saritasa.com>
2022-01-27 21:39:49 +0500

    Add project files.

---
+++ InstaBlaster/Controllers/LinksController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaBlaster.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LinksController : ControllerBase
+    {
+        
+    }
+}
